aa6b7741bc211077a7ba6975fc39d58e087edd36
Travis Pritchard <pritchard.travis@deq.state.or.us>
2021-07-09 11:57:40 -0700

    Documenting updates

--- R/unique_AU.R
+++ R/unique_AU.R
@@ -1,10 +1,10 @@
 #' unique_AU
 #'
-#' This function extarcts the unique element from an AU_ID. It will extract the last element defines by the '_' symbol.
+#' This function extracts the unique element from an AU_ID. It will extract the last element defines by the '_' symbol.
 #'
 #' @param AU_ID AU_ID string to extract unique code portion from
 #' @return THe unique portion of an AU_ID as string
-#' @example unique_AU('OR_WS_170501100706_05_102965')
+#' @examples unique_AU('OR_WS_170501100706_05_102965')
 #' @export
 
 
