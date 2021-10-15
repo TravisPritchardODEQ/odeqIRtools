4281560a56de77d7d5485940711b2b469f3d1621
Travis Pritchard <pritchard.travis@deq.state.or.us>
2021-10-15 11:09:18 -0700

    Add function to join to previopus assessment conclusions

---
+++ R/join_prev_assessments.R
@@ -0,0 +1,55 @@
+#' join_prev_assessments
+#'
+#' This function joins in previous assessment conclusions to current assessment process.
+#' If AY_type == 'WS', the data will be joined to previous impairements bu AU_GNIS as well as overall AU categorization.
+#' IF AU_type is 'other', the data will be jopined to overall AU assessment conclusion.
+#'
+#' @param df Dataframe of new assessment conclusions
+#' @param AU_type What sort of AU type is the join opertaing on "WS" or Other"
+#' @export
+
+
+
+
+
+
+join_prev_assessments <- function(df, AU_type){
+
+  # test dataset ----------------------------------------------------------------------------------------------------
+
+  df <- other_category
+  AU_type <- "Other"
+
+  if(AU_type == "WS"){
+
+
+    df_names <- names(df)
+
+
+    GNIS_join <- df %>%
+      mutate(AU_GNIS = str_c(AU_ID, AU_GNIS_Name, sep = ";"),
+             Pollu_ID = as.character(Pollu_ID)) %>%
+      left_join(WS_GNIS_previous_listings) %>%
+      select(all_of(df_names), GNIS_previous_IR_impairement)
+
+    GNIS_join_names <- names(GNIS_join)
+
+    overall_join <- GNIS_join %>%
+      left_join(overall_category) %>%
+      select(all_of(GNIS_join_names), AU_previous_IR_category)
+  } else {
+
+    # non-watershed ---------------------------------------------------------------------------------------------------
+
+    df <- other_category
+
+    df_names <- names(df)
+    overall_join <- df %>%
+      mutate(Pollu_ID = as.character(Pollu_ID)) %>%
+      left_join(overall_category) %>%
+      select(all_of(df_names), AU_previous_IR_category)
+
+  }
+
+  return(overall_join)
+}
---
+++ data-raw/2018 previous listings.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+library(openxlsx)
+
+
+# Bring in previous listings --------------------------------------------------------------------------------------
+
+WS_GNIS_previous_listings_import <- read.xlsx("//deqhq1/WQASSESSMENT/2022IRFiles/Code/Watershed_Impairements_parameters.xlsx")
+
+WS_GNIS_previous_listings <- WS_GNIS_previous_listings_import %>%
+  select(AU_ID, Char_Name, AU_GNIS, Pollu_ID, wqstd_code, Period, IR_category) %>%
+  rename(GNIS_previous_IR_impairement = IR_category)
+
+
+
+AU_previous_categories_import <- read.csv("E:/Documents/IR2018/ATTAINS/Rollup/Basin_categories/ALL BASINS_Parameters.csv")
+
+AU_previous_categories <- AU_previous_categories_import %>%
+  select(AU_ID, Char_Name,  Pollu_ID, wqstd_code, Period, IR_category) %>%
+  rename(AU_previous_IR_category = IR_category) %>%
+  mutate(Pollu_ID = as.character(Pollu_ID))
+
+
+
+usethis::use_data(WS_GNIS_previous_listings, overwrite = TRUE)
+usethis::use_data(AU_previous_categories, overwrite = TRUE)
