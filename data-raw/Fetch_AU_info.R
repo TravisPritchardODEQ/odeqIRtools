2541b3a57ebd6fa66a1ad0c9bfd2a38d320584ea
Travis Pritchard <pritchard.travis@deq.state.or.us>
2024-04-10 11:27:59 -0700

    Updating AU info

--- data-raw/Fetch_AU_info.R
+++ data-raw/Fetch_AU_info.R
@@ -3,23 +3,27 @@ library(tidyverse)
 arc.check_product()
 
 
+#
+# rivstream <- arc.select(arc.open('https://services.arcgis.com/uUvqNMGPm7axC2dD/ArcGIS/rest/services/IR_2022_Final/FeatureServer/34')) |>
+#   select(AU_ID, AU_Name,AU_UseCode, HUC12)
+#
+# waterbody <- arc.select(arc.open('https://services.arcgis.com/uUvqNMGPm7axC2dD/ArcGIS/rest/services/IR_2022_Final/FeatureServer/43')) |>
+#   select(AU_ID, AU_Name,AU_UseCode, HUC12)
+#
+# watershed <-  arc.select(arc.open('https://services.arcgis.com/uUvqNMGPm7axC2dD/ArcGIS/rest/services/IR_2022_Final/FeatureServer/44')) |>
+#   select(AU_ID, AU_Name,AU_UseCode, HUC12)
+#
+#
+# AU_info <- bind_rows(rivstream, waterbody, watershed) |>
+#   filter(!is.na(HUC12)) |>
+#   distinct()
+#
 
-rivstream <- arc.select(arc.open('https://services.arcgis.com/uUvqNMGPm7axC2dD/ArcGIS/rest/services/IR_2022_Final/FeatureServer/34')) |>
+
+
+
+AU_info <- arc.select(arc.open('https://services.arcgis.com/uUvqNMGPm7axC2dD/arcgis/rest/services/Oregon_AUs/FeatureServer/3')) |>
   select(AU_ID, AU_Name,AU_UseCode, HUC12)
-
-waterbody <- arc.select(arc.open('https://services.arcgis.com/uUvqNMGPm7axC2dD/ArcGIS/rest/services/IR_2022_Final/FeatureServer/43')) |>
-  select(AU_ID, AU_Name,AU_UseCode, HUC12)
-
-watershed <-  arc.select(arc.open('https://services.arcgis.com/uUvqNMGPm7axC2dD/ArcGIS/rest/services/IR_2022_Final/FeatureServer/44')) |>
-  select(AU_ID, AU_Name,AU_UseCode, HUC12)
-
-
-AU_info <- bind_rows(rivstream, waterbody, watershed) |>
-  filter(!is.na(HUC12)) |>
-  distinct()
-
-
-
 
 usethis::use_data(AU_info, overwrite = TRUE)
 
