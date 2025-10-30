c46dd42aeb0ad631f9e60f1ca9670e701ef0dac4
Travis Pritchard <travis.pritchard@deq.oregon.gov>
2025-10-30 14:45:35 -0700

    Bug fix

--- data-raw/get ggregate data.R
+++ data-raw/get ggregate data.R
@@ -2,7 +2,7 @@
 
 #Load in file created in cycle repository
 
-aggregate_data <- load("C:/Users/tpritch/OneDrive - Oregon/R Projects/IR_2026/Validation/2026_aggregate_data.Rdata")
+load("C:/Users/tpritch/OneDrive - Oregon/R Projects/IR_2026/Validation/2026_aggregate_data.Rdata")
 
 
 #Save in package
